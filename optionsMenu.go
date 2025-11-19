b6fe6806cae95246e9017ffa36942f3b28a28af1
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-19 03:06:00 +0200

    Created OptionsMenu

---
+++ optionsMenu.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+
+func optionsMenu(){
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Transactions")
+	fmt.Println("5. Exit")
+}
