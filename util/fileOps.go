64c255d1162721651585b492438fdd1c2057c12a
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-19 03:13:16 +0200

    Separated file operations into a new file with new package

---
+++ util/fileOps.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func WriteFloatToFile(value float64, fileName string){
+	textValue := fmt.Sprint(value)
+	os.WriteFile(fileName, []byte(textValue), 0644)
+}
+
+func GetFloatFromFile(fileName string, defaultValue float64) (float64, error) {
+	data, err :=os.ReadFile(fileName)
+	if err != nil{
+		return defaultValue, errors.New("failed to find file")
+	}
+	textValue := string(data)
+	value, err := strconv.ParseFloat(textValue, 64)
+	if err != nil{
+		return defaultValue, errors.New("failed to parse stored value")
+	}
+	return value, nil
+}
